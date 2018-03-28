5ec5e25581a0e5a649d8a05726030241ff751c7e
chethanank <chethana.nk@globallogic.com>
2018-03-28 13:29:46 +0530

    initial commit

---
+++ src/test/java/com/gl/EurekaServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.gl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class EurekaServerTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
