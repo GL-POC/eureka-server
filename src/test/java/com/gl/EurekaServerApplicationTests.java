5effec9d6e1f13e37c5ab8a182342e609e82c1e0
K Bharath <K.Bharath@synapse.com>
2018-03-29 11:22:11 +0530

    Fix SpringBoot tests

--- src/test/java/com/gl/EurekaServerApplicationTests.java
+++ src/test/java/com/gl/EurekaServerApplicationTests.java
@@ -1,12 +1,14 @@
 package com.gl;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-public class EurekaServerTests {
+public class EurekaServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
